a9b8f7ab50e0363535b83cfb1ac676f74591ec1e
zakv <zakven@mit.edu>
2017-04-14 19:38:51 -0400

    Updated plot_basis_vectors() to not assume images are 121x121 pixels

--- AbsorptionImageProcessing/plot_basis_vectors.m
+++ AbsorptionImageProcessing/plot_basis_vectors.m
@@ -1,13 +1,29 @@
-function plot_basis_vectors( basis, indices )
+function plot_basis_vectors( basis, indices, image_size)
 %Plots image_in with a colorbar
 %   basis should be a 2D array of basis vectors from get_basis()
-%   indices should be a 1D array of indices
+%
+%   indices should be a 1D array of indices of the basis vectors to plot
+%   (i.e. 1:3 will plot the first three basis vectors)
+%
+%   image_size is an optional argument.  When given should be a vector
+%   giving the dimensions of the image.  If it is not given, the iamges are
+%   assumed to be 121x121.
+%
+%   Example Usage:
+%   >> plot_basis_vectors(basis, 1:10, [121,121]);
+
+%Set default image size
+if nargin<3
+    image_size=121;
+end
+
+%Now plot the basis vectors
 for index=indices
     figure();
     vector=basis(:,index);
     %There's probably a smarter way to do the next line, but I'm not sure
     %exactly what that way is right now
-    image_in=reshape(vector,121,121);
+    image_in=reshape(vector,image_size);
     image(image_in,'CDataMapping','scaled');
     plot_name=sprintf('Basis Vector #%d',index);
     title(plot_name,'Interpreter','none');
